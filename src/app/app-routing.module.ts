b2db172cb79fb29b5decec571d02b96e65d9edbf
ColombusAH <Hattabavner@gmail.com>
2019-11-18 09:32:37 +0200

    change to hebrew language

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: '**', component: HomeComponent }
-];
+const routes: Routes = [{ path: '', component: HomeComponent }, {path: '**', component: HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
